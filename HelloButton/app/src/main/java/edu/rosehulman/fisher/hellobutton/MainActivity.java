ef0460c662f0d88ebcf3edf61cd674e6832f3a08
David Fisher <fisherds@rose-hulman.edu>
2018-03-06 16:00:58 -0500

    Added a button listener and text view connection

--- HelloButton/app/src/main/java/edu/rosehulman/fisher/hellobutton/MainActivity.java
+++ HelloButton/app/src/main/java/edu/rosehulman/fisher/hellobutton/MainActivity.java
@@ -2,12 +2,30 @@ package edu.rosehulman.fisher.hellobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private int count = 0;
+  private Button button;
+  private TextView textView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    button = (Button) findViewById(R.id.press_me_button);
+    textView = (TextView) findViewById(R.id.message_text_view);
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        count++;
+        textView.setText(getResources().getQuantityString(R.plurals.message_format, count, count));
+      }
+    });
   }
 }
